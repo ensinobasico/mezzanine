a6e0adc82b25c572437748c6a69673a9ff653bdb
ensinobasico <davidc.azevedo@gmail.com>
2014-04-24 13:48:46 +0200

    Update local_settings.py

--- mezzanine/project_template/deploy/local_settings.py
+++ mezzanine/project_template/deploy/local_settings.py
@@ -37,16 +37,16 @@ CACHES = {
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 FABRIC = {
     "SSH_USER": "", # SSH username for host deploying to
-    "HOSTS": ALLOWED_HOSTS[:1], # List of hosts to deploy to (eg, first host)
+    "HOSTS": ['yeslow.com'], # List of hosts to deploy to (eg, first host)
     "DOMAINS": ALLOWED_HOSTS, # Domains for public site
-    "REPO_URL": "ssh://hg@bitbucket.org/user/project", # Project's repo URL
-    "VIRTUALENV_HOME": "", # Absolute remote path for virtualenvs
+    "REPO_URL": "https://github.com/ensinobasico/mezzanine/edit/master/mezzanine/project_template", # Project's repo URL
+    "VIRTUALENV_HOME": "/www/home", # Absolute remote path for virtualenvs
     "PROJECT_NAME": "", # Unique identifier for project
     "REQUIREMENTS_PATH": "requirements.txt", # Project's pip requirements
     "GUNICORN_PORT": 8000, # Port gunicorn will listen on
     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
-    "DB_PASS": "", # Live database password
-    "ADMIN_PASS": "", # Live admin user password
+    "DB_PASS": "admin", # Live database password
+    "ADMIN_PASS": "admin", # Live admin user password
     "SECRET_KEY": SECRET_KEY,
     "NEVERCACHE_KEY": NEVERCACHE_KEY,
 }
