4d80a7da61314ae7589a5b497ab55a3f01809cc0
ensinobasico <davidc.azevedo@gmail.com>
2014-04-24 14:14:24 +0200

    Update local_settings.py

--- mezzanine/project_template/local_settings.py
+++ mezzanine/project_template/local_settings.py
@@ -24,7 +24,7 @@ DATABASES = {
 FABRIC = {
     "SSH_USER": "", # SSH username for host deploying to
     "HOSTS": ['188.226.188.21'], # List of hosts to deploy to (eg, first host)
-#    "DOMAINS": , # Domains for public site
+    "DOMAINS": ['yeslow.com'], # Domains for public site
     "REPO_URL": "https://github.com/ensinobasico/mezzanine/edit/master/mezzanine/project_template", # Project's repo URL
     "VIRTUALENV_HOME": "/www/home", # Absolute remote path for virtualenvs
     "PROJECT_NAME": "", # Unique identifier for project
